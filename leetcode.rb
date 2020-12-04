82f6b0015c5967ba444f70482c049b88d4c4f3cd
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2020-12-04 09:56:29 -0700

    Leetcode 1480

---
+++ leetcode.rb
@@ -0,0 +1,11 @@
+# # Running sum of 1d array
+# Input: nums = [1,2,3,4]
+# Output: [1,3,6,10]
+# Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+def running_sum(nums)
+  sum = 0
+  nums.map do |num|
+    sum += num
+  end
+end
