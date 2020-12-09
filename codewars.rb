a54c8592efa7a129ba2c768f6e271b6275c59ffa
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2020-12-08 20:18:07 -0700

    Codewars filter the number

--- codewars.rb
+++ codewars.rb
@@ -5,7 +5,14 @@ def XO(str)
 end
 
 # Split in Parts 7kyu
+# The aim of this kata is to split a given string into different strings of equal size (note size of strings is passed to the method)
 def split_in_parts (s, part_length)
   split = s.chars.each_slice(part_length).map(&:join)
   split.join(' ')
+end
+
+# Filter the number
+# Oh no! The number has been mixed up with the text. Your goal is to retreive the number from the text, can you return the number back to it's original state?
+def filter_string(string)
+  string.gsub(/['aA-zZ', ' ']/, '').to_i
 end
