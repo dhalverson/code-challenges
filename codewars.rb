2167e56ac9964c1f790678a0a4471a5a04babfcc
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2020-12-13 19:33:51 -0700

    Codewars all star code challenge 22

--- codewars.rb
+++ codewars.rb
@@ -77,3 +77,15 @@ def hydrate(s)
     "#{total} glasses of water"
   end
 end
+
+
+# All Start Code Challenge #22
+# Create a function called toTime() that takes an integer argument of seconds and converts the value into a string describing how many hours and minutes comprise that many seconds.
+
+# Any remaining seconds left over are ignored.
+
+def to_time(seconds)
+  hours = seconds / 3600
+  minutes = (seconds / 60) % 60
+  "#{hours} hour(s) and #{minutes} minute(s)"
+end
