4c9718de9efc5eab2eed3824a648c8e22b62f84d
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2021-03-16 12:21:51 -0600

    Codewars string repeat

---
+++ Codewars/string_repeat.rb
@@ -0,0 +1,7 @@
+# Write a function called repeat_string which repeats the given string str exactly count times.
+
+def repeat_str(repeat, string)
+  string * repeat
+end
+
+print repeat_str(4, 'hello')
